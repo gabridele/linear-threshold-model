4afad1b65e00debb9482869f012e18fe367047a8
gabridele <gabrieledele@gmail.com>
2024-07-23 20:59:05 +0200

    Update restore_parallel.sh

--- restore_parallel.sh
+++ restore_parallel.sh
@@ -4,16 +4,16 @@ function restore {
     arg1="$1"
     sub_id=$(basename "$arg1" | grep -oP 'sub-\d+')
     n_seed=$(basename "$arg1" | awk -F '_' '{print $NF}' | cut -d 's' -f 1)
-    arg2="${arg1%association_matrix*}zero_connection_nodes_${sub_id}_${n_seed}seeds.csv"
+    arg2="${arg1%association_matrix*}removed_nodes_${sub_id}_2seeds.csv"
     
     echo -e "############# $sub_id, $arg1, $arg2"
-    python ../code/linear-threshold-model/restore_zscore_assoc_mtrx.py $arg1 $arg2 $n_seed
+    python ../code/linear-threshold-model/restore_assoc_mtrx.py $arg1 $arg2 $n_seed
 
 }
 
 export -f restore
 
-find "$path_der" -type f -name '*association_matrix_*_30seeds.csv' > "$path_der/ass_mtrx_files.txt"
+find "$path_der" -type f -name 'association_matrix_*_2seeds.csv' > "$path_der/ass_mtrx_files.txt"
 
 N=140
 (
