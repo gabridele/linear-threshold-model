d2185d466d40fef4b2647359c0af0de205f0683e
gabridele <gabrieledele@gmail.com>
2024-10-15 14:17:37 +0200

    Update linear_threshold_model_association.py

--- linear_threshold_model_association.py
+++ linear_threshold_model_association.py
@@ -207,11 +207,7 @@ def main(input_file_path, n_pop):
     
     print(f"now processing: {sub_id} with {n_pop} seeds competitive scenario")
     
-<<<<<<< HEAD
-    # identify zero connections and <= 5 connection nodes
-=======
     # identify zero connections and < 5 connection nodes (Seguin, Jedynak, et al., 2023)
->>>>>>> d8421755fed973179feefa9fa714243ec0493f7f
     zero_rows = np.where(np.sum(adj_matrix, 0) == 0)[0].tolist()
     low_connection_nodes = np.where(np.sum(adj_matrix > 0, axis=0) < 5)[0].tolist()
     
